4bf34df5a1a9574170425ba2081c63e6595e1476
Dax2976 <98029197+Dax2976@users.noreply.github.com>
2022-06-09 15:29:58 +0200

    commit con errore

---
+++ resources/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header>
+    <div class="headerBluetop">
+        <span>DC POWER VISA</span>
+        <span>ADDITIONAL DC SITES</span>
+    </div>
+    <div class="">
+        <nav>
+            <img src="{{assets('images/dc-logo.png')}}" alt="">
+            <ul>
+                <li>
+                    <a href="">characters</a>
+                </li>
+                <li>
+                    <a href="">comics</a>
+                </li>
+                <li>
+                    <a href="">movies</a>
+                </li>
+                <li>
+                    <a href="">tv</a>
+                </li>
+                <li>
+                    <a href="">games</a>
+                </li>
+                <li>
+                    <a href="">collectibles</a>
+                </li>
+                <li>
+                    <a href="">videos</a>
+                </li>
+                <li>
+                    <a href="">fans</a>
+                </li>
+                <li>
+                    <a href="">news</a>
+                </li>
+                <li>
+                    <a href="">shop</a>
+                </li>
+            </ul>
+            <input type="text" placeholder="Search">
+        </nav>
+    </div>
+</header>
---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>home</title>
+    <link rel="stylesheet" href="{{'css/app.css'}}">
+</head>
+
+<body>
+    @include('partials.header')
+</body>
+
+</html>
